a1b338673a8655d15e06df236fe163ee0d9a3220
yoshida0261 <yoshida261@gmail.com>
2016-01-11 00:00:20 +0900

    データのList表示用

---
+++ app/src/main/java/stcompany/com/conferenceroomreservation/ListActivity.java
@@ -0,0 +1,13 @@
+package stcompany.com.conferenceroomreservation;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class ListActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
